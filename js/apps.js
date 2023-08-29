8d9a5291f99c3ca6186c8ab6e1f01cf706e5f600
mamunawlad <133993137+mamunawlad@users.noreply.github.com>
2023-08-30 00:49:57 +0600

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,19 @@
+$(function () {
+
+
+$(`.popBtn`).click (function () {
+
+
+$(`.popup`).addClass(`show`)
+
+
+
+});
+
+$(`.crossBtn`).click(function () {
+
+$(`.popup`).removeClass(`show`)
+
+})
+
+})
